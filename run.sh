ed033d2924e9d363156b448fab7633b4783e3753
grandmullah <collinsbiegon9@gmail.com>
2023-06-19 08:54:58 +0300

    update

--- run.sh
+++ run.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
-ls 
+
 cargo build --release
+ls  
+exec ./target/release/node-template --chain ./customSpecRaw.json --base-path /tmp/node1 --keystore-path /tmp/node1/chains/radicle_testnet/keystore  --port 30333 --ws-port 9944 --unsafe-ws-external --rpc-port 9933 --unsafe-rpc-external --rpc-cors all --validator--name node1
