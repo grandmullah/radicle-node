a40dcc47a0688587e884e04e3da4a7fc5d262942
grandmullah <collinsbiegon9@gmail.com>
2023-07-11 19:24:29 +0300

    complete  reward pallet

--- pallets/reward/src/lib.rs
+++ pallets/reward/src/lib.rs
@@ -19,7 +19,7 @@ pub use pallet::*;
 #[frame_support::pallet]
 pub mod pallet {
 	use super::*;
-	use frame_support::{pallet_prelude::*,sp_runtime::traits::{AtLeast32BitUnsigned, Zero}, RuntimeDebug,
+	use frame_support::{pallet_prelude::*,sp_runtime::traits::{AtLeast32BitUnsigned,Saturating, Zero, CheckedAdd, CheckedSub,}, RuntimeDebug,
 	};
 	use frame_system::pallet_prelude::{*, OriginFor};
 	const STORAGE_VERSION: frame_support::traits::StorageVersion =
@@ -81,36 +81,76 @@ pub mod pallet {
 
 
 	#[pallet::event]
+	#[pallet::generate_deposit(pub(super) fn deposit_event)]
 	pub enum Event<T: Config<I>, I: 'static = ()> {
-
+		Transfered(T::AccountId, T::AccountId, T::Balance),
 	}
 
-	// #[pallet::error]
+	#[pallet::error]
+	pub enum Error<T, I = ()> {
+		/// An account would go below the minimum balance if the operation were executed.
+		BelowMinBalance,
+		// The origin account does not have the required permission for the operation.
+		NoPermission,
+		/// An operation would lead to an overflow.
+		Overflow,
+		/// An operation would lead to an underflow.
+		Underflow,
+		/// Cannot burn the balance of a non-existent account.
+		CannotBurnEmpty,
+		/// There is not enough balance in the sender's account for the transfer.
+		InsufficientBalance,
+	}
 	
 	#[pallet::call]
 	impl<T: Config<I>, I: 'static> Pallet<T, I> {
 
 		#[pallet::call_index(0)]
 		#[pallet::weight(0)]
-		pub fn accept_ride(
+		pub fn transfer(
 			origin: OriginFor<T>,
+			to: T::AccountId,
+			#[pallet::compact] amount: T::Balance,
 			//ride Id 
 		) -> DispatchResultWithPostInfo {
-			let _signer = ensure_signed(origin)?;
-			
+			let signer = ensure_signed(origin)?;
+			Accounts::<T,I>::try_mutate(&signer, |bal| -> DispatchResult {
+				let new_bal = bal.checked_sub(&amount).ok_or(Error::<T,I>::InsufficientBalance)?;
+				
+				*bal = new_bal;
+				Ok(())
+			})?;
+
+			Accounts::<T,I>::try_mutate(&to, |rec_bal| -> DispatchResult {
+				let new_bal = rec_bal.saturating_add(amount);
+				
+				*rec_bal = new_bal;
+				Ok(())
+			})?;	
+			Self::deposit_event(Event::<T,I>::Transfered(signer, to, amount));		
 			Ok(Pays::No.into())
 		}
 
 
 		
 		// mint 
+		//transfer 
 
 
 	}
 	impl<T: Config<I>, I: 'static> Pallet<T, I> {
-
+		fn mint_into(who: &T::AccountId, amount: T::Balance)->bool {
+			Accounts::<T,I>::mutate(&who, |bal| {
+				let created = bal == &Zero::zero();
+				
+				// meta.issuance = meta.issuance.checked_add(&amount).ok_or(Error::<T>::Overflow)?;
+				// fine because we check the issuance for overflow before minting and transfers
+				// don't change the issuance
+				*bal = bal.saturating_add(amount);
+				created
+			})
+		}
 	}
-
 
 
 }
