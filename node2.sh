f1797066b677b0be4f6451ca97afee9fe85dcbbb
grandmullah <collinsbiegon9@gmail.com>
2023-07-17 15:02:22 +0300

    update

--- node2.sh
+++ node2.sh
@@ -3,4 +3,4 @@
 
 cargo build --release
 ls  
-exec ./target/release/node-template --chain ./customSpecRaw.json --base-path /tmp/node2 --keystore-path /tmp/node2/chains/radicle_testnet/keystore  --port 30334 --ws-port 9945 --unsafe-ws-external --rpc-port 9934 --unsafe-rpc-external --rpc-cors all --validator --name node2 --bootnodes /ip4/0.0.0.0/tcp/30333/p2p/12D3KooWQAkseRwSu5EcuFRokMLiypSPuyaXNgfHEJC7bqCB8ZRY
+exec ./target/release/node-template --chain ./customSpecRaw.json --base-path /tmp/node2 --keystore-path /tmp/node2/chains/radicle_testnet/keystore  --port 30334 --ws-port 9945 --unsafe-ws-external --rpc-port 9934 --unsafe-rpc-external --rpc-cors all --validator --name node2 --bootnodes /ip4/0.0.0.0/tcp/30333/p2p/12D3KooWNfNmKNyemvGxLh9TDdT3LTUCFA2xf2xaTn64scXbGqsM
